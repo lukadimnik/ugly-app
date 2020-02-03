6aa3e34988256cc1b03d2eb363fea0270261f17f
Luka Dimnik <luka.dimnik88@gmail.com>
2020-02-03 11:40:35 +0200

    added comments and other changes

--- UglyWebApp.js
+++ UglyWebApp.js
@@ -1,3 +1,6 @@
+// use of const - only the outer layer e.g. first bracked is constant.
+// content of array can be manipulated
+// use of trailing comma
 const costTypes = [
   {
     type: "daily",
@@ -14,9 +17,10 @@ const costTypes = [
   {
     type: "yearly",
     days: 365
-  }
+  },
 ];
 
+// adding object as another objects property
 const costs = [
   {
     name: "rent",
@@ -44,7 +48,9 @@ const costs = [
     costType: costTypes[3]
   }
 ];
-
+// use of arrow function with brackets
+// not using brackets when having only one 
+// saving a function in a variable
 const getValue = elementId => {
   let valueAsText = document.getElementById(elementId).value;
   return Number(valueAsText);
@@ -54,7 +60,8 @@ const getName = elementId => {
   let valueAsText = document.getElementById(elementId).value;
   return valueAsText;
 };
-
+// use of let
+// use of spread
 let anotherCostsArray = [...costs];
 let sillyArray = [...costs];
 
@@ -63,11 +70,13 @@ const addCost = () => {
   let amount = getValue("amount");
   let costTypeIndex = getValue("costType");
 
+// adhoc creating new object
+// creating new object properties in a new way - same as let newObject = {name: name, amount: amount}
   let newObject = {
     name,
     amount
   };
-
+// adding new attribute attribute to object
   newObject.costType = costTypes[costTypeIndex].type;
 
   let anotherObject = {
@@ -76,23 +85,69 @@ const addCost = () => {
     costType: costTypes[costTypeIndex]
   };
 
+  // creating object by naming properties based on values of the variable
   let sillyObject = {
     [name]: name,
     [amount]: amount
   };
+
+  // use of .push()
   anotherCostsArray.push(newObject);
   costs.push(anotherObject);
   sillyArray.push(sillyObject);
 
+  // use of .forEach()
   costs.forEach(cost => {
     let myElement = document.createElement("p");
+    // template literals
     myElement.innerText = ` name: ${cost.name}, amount: ${cost.amount}, cost type: ${cost.costType.type}`;
     document.body.appendChild(myElement);
   });
 
+  // use of .filter()
+  let filteredArray = sillyArray.filter((_, index) => index % 2);
+
+  console.log("filtered array", filteredArray);
+
   console.log("another costs array: ", anotherCostsArray);
 };
 
+// use of immediately executing function
+// use of implicit return in arrow function
 (par => console.log(par))("JIIIIIIHHHAAAAAAA");
 
-(costTypes.map((cost) => console.log(`Name: ${cost.type} , Days:${cost.type}`))(costTypes)
+// using .map()
+(()=>costTypes.map(cost => console.log(`Name: ${cost.type} , Days:${cost.type}`)))
+(costTypes);
+
+// class definition
+// extends creates a child Class from a parent Class
+class Vehicle {
+  name;
+  constructor(){
+    this.name = "lorem ipsum";
+  }
+}
+
+class Car extends Vehicle {
+  color;
+  brand;
+  constructor(color, brand){
+    super(name);
+    this.color = color;
+    this.brand = brand;
+  }
+
+  getColor(){
+    return this.color;
+  }
+  getBrand(){
+    return this.brand;
+  }
+  setColor(color){
+    this.color = color;
+  }
+}
+
+let myCar = new Car("red", "BMW");
+console.dir(myCar);

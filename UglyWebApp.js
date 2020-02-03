6de603fb1e68547c45994a2300c24f867fbdd7c5
Luka Dimnik <luka.dimnik88@gmail.com>
2020-02-03 16:16:33 +0200

    added new ES6 functionalities

--- UglyWebApp.js
+++ UglyWebApp.js
@@ -60,10 +60,12 @@ const getName = elementId => {
   let valueAsText = document.getElementById(elementId).value;
   return valueAsText;
 };
-// use of let
 // use of spread
-let anotherCostsArray = [...costs];
-let sillyArray = [...costs];
+const anotherCostsArray = [...costs];
+const sillyArray = [...costs];
+// adding coppies of two arrays. Added string to the array
+const extraSillyArray = [...anotherCostsArray, ...sillyArray, "muahahahaha",];
+console.log(extraSillyArray);
 
 const addCost = () => {
   let name = getName("name");
@@ -86,10 +88,16 @@ const addCost = () => {
   };
 
   // creating object by naming properties based on values of the variable
+  // creating an object method in a new way
   let sillyObject = {
     [name]: name,
-    [amount]: amount
+    [amount]: amount,
+    helo(){
+      return console.log("returned value from helo function");
+    }
   };
+
+  sillyObject.helo();
 
   // use of .push()
   anotherCostsArray.push(newObject);
@@ -98,6 +106,7 @@ const addCost = () => {
 
   // use of .forEach()
   costs.forEach(cost => {
+    // let variable declared inside a function (its not visible outside the block)
     let myElement = document.createElement("p");
     // template literals
     myElement.innerText = ` name: ${cost.name}, amount: ${cost.amount}, cost type: ${cost.costType.type}`;
@@ -105,7 +114,8 @@ const addCost = () => {
   });
 
   // use of .filter()
-  let filteredArray = sillyArray.filter((_, index) => index % 2);
+  // using only the second parameter in a function
+  let filteredArray = sillyArray.filter((_, index) => index >= 2);
 
   console.log("filtered array", filteredArray);
 
@@ -151,3 +161,47 @@ class Car extends Vehicle {
 
 let myCar = new Car("red", "BMW");
 console.dir(myCar);
+
+// function that returns a function
+// use of default value for parameter if no parameter passed to a function
+const FuncRetursAFunc = (word="dede") => {
+  let firstWord = word;
+  //function with two parameters
+      return (secondWord, thirdWord) => console.log(`${firstWord} , ${secondWord} , ${thirdWord}`);
+  
+};
+FuncRetursAFunc()("jaaaa", "jojo");
+//both parameters passed
+FuncRetursAFunc("neeee")("jaaaa","jojo");
+
+// objects
+let john = {height: 175, nationality: "english"};
+// overwriting the object property
+let joe = {...john, nationality: "french"};
+console.log(john);
+console.dir(joe);
+
+// destructuring
+let batman = {height: 180, weight: 90, realName: "Bruce Wayne"};
+
+let {height, weight, realName} = batman;
+console.log(height, weight, realName);
+
+// hoisted variable var
+// if (1){
+//   hoistedName = "Luka";
+
+//   function funnyFunc(){
+//     var hoistedNamex = "Noe";
+//    return 1 ? hoistedNamex = "Nigel" : hoistedNamex = "Fred";
+//   }
+//   funnyFunc();
+// }
+
+// console.log(hoistedName);
+
+// ternary operator
+// 0 is a falsy value
+var someVar = 0;
+someVar ? someVar = 1: someVar = 2;
+console.log(someVar);
